96eab90e2af76c207be8bd8e2e8fe868ee8b64fa
krishnkant-singh <krishnakantsksingh@gmail.com>
2023-07-01 23:54:42 +0530

    without firebase using LocalStorage

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-import {initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase,ref,push,onValue,remove} from"https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-const appSetting={
-    databaseURL:"https://playground-bfd43-default-rtdb.firebaseio.com"
-}
-
-const app=initializeApp(appSetting)
-console.log(app)
-const database=getDatabase(app)
-const itemsInDb=ref(database,"items")
 
 const inputField=document.getElementById("input-field")
 const addButton=document.getElementById("add-button")
 const shoppingList=document.getElementById("shopping-list")
+var reloadIconPressed = false;
+
+onReload()
+
+
 addButton.addEventListener("click", (e) => {
 	e.preventDefault();
     let inputValue=inputField.value
     if(inputValue!=''){
-    push(itemsInDb,inputValue)
-    //appendItemsToShoppingListEl(inputValue)
+    appendItemsToShoppingListEl(inputValue)
     clearInputFieldEl()	
     console.log(`${inputValue} added to database`)
     } else{
@@ -26,53 +19,57 @@ addButton.addEventListener("click", (e) => {
     }
 })
 
-onValue(itemsInDb,function(snapshot) {
 
-    if(snapshot.exists()){
-    let itemsArray=Object.entries(snapshot.val())
-    clearItemsEl()
-        for(let i=0;i<itemsArray.length;i++){
-            let itemsinDb=itemsArray[i];
-            let currentItemId =itemsinDb[0];
-            let currentItemValue=itemsinDb[1];
-            console.log(currentItemId)
-            
-            appendItemsToShoppingListEl(itemsinDb)
-        //console.log(itemsArray[i])
-    }
-    //console.log(snapshot.val())
-}
-else{
-    shoppingList.innerHTML="NO ITEMS"
-
-}
-})
 function clearInputFieldEl(){
     inputField.value="";
 }
 function clearItemsEl(){
     shoppingList.innerHTML=""
 }
+function onReload() {
+    clearItemsEl()
+    // Your custom code here
+    console.log("Window is being reloaded");
+    var storedData = localStorage.getItem("item");
+    var parsedArray = storedData ? JSON.parse(storedData) : [];
+    //clearItemsEl();
+
+    var items = [];
+    if(parsedArray.length!=0){
+            var news=localStorage.getItem("item");
+            for(let i=0;i<parsedArray.length;i++){
+                shoppingList.innerHTML+= `<li>${parsedArray[i]}</li>`;
+        }   
+    }
+ }
 
 function appendItemsToShoppingListEl(inputValue){
-   // shoppingList.innerHTML+=`<li>${inputValue}</li>`
-   let itemId=inputValue[0];
-   let itemsValue=inputValue[1];
 
-
-   let newEl=document.createElement("li")
-   newEl.textContent=itemsValue
-  
-
-
-   newEl.addEventListener("click",function() {
+    var storedArray = localStorage.getItem("item");
+    var parsedArray = storedArray ? JSON.parse(storedArray) : [];
+    parsedArray.push(`${inputValue}`);
    
-   let exactLocationOfItemInDb=ref(database,`items/${itemId}`)
-   remove(exactLocationOfItemInDb)
-   
+    var jsonString = JSON.stringify(parsedArray);
+    var parsedArray = JSON.parse(jsonString);
+    localStorage.setItem("item", JSON.stringify(parsedArray));
+    console.log(localStorage.getItem("item[0]"))
+    shoppingList.innerHTML+= `<li>${inputValue}</li>`;
 
-
-   })
-   shoppingList.append(newEl)
 }
+var shoppinglist = document.getElementById("shopping-list");
+shoppinglist.addEventListener("click", function(event) {
+  var clickedElement = event.target;
+  if (clickedElement.nodeName === "LI" && clickedElement.parentNode === shoppinglist) {
+    shoppinglist.removeChild(clickedElement);
+    var storedArray = localStorage.getItem("item");
+   var parsedArray = storedArray ? JSON.parse(storedArray) : [];
+    var elementIndex = parsedArray.indexOf(clickedElement.textContent);
+    if (elementIndex > -1) {
+        parsedArray.splice(elementIndex, 1);
+      }
+      localStorage.setItem("item", JSON.stringify(parsedArray));
+
+  }
+});
+
 
